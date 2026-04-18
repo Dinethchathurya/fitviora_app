63fba5384ac9e6e97dd8b49ca4bfd9e7273d48db
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2026-04-19 00:49:58 +0530

    test new release

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/app.dart';
-import 'firebase_options.dart'; // ✅ REQUIRED
+import 'firebase_options.dart'; 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
