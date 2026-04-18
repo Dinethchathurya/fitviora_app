bb233ff8e1943b6044dacfbe4041ef727c506753
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2026-04-18 23:38:29 +0530

    firebase connect and functions runs smoothly

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'app/app.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   // try {
+
+//     await Firebase.initializeApp(
+//         options: DefaultFirebaseOptions.currentPlatform,
+//     );
+
+//   // } catch (e) {
+
+//   //   debugPrint("Firebase init error: $e");
+
+//   // }
+//   runApp(const FitVioraApp());
+// }
+
+
+
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/app.dart';
+import 'firebase_options.dart'; // ✅ REQUIRED
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp();
-  } catch (_) {
-    // Firebase config can be added later.
-  }
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const FitVioraApp());
 }
